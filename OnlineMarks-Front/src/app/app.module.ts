ff81d9ed57ae08da36c56fdb3fdca2672d5e1eba
malesdejan54 <malesdejan54@gmail.com>
2020-08-11 14:51:37 +0200

    initial commit

---
+++ OnlineMarks-Front/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomePageComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
