0dad6c78fb2461818a1e08982c8c2d35d97c68a6
Marko Mitic <m.mitic@levi9.com>
2020-08-11 14:37:25 +0200

    Db context implemetation

---
+++ OnlineMarks/OnlineMarks.Data.Models/User.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineMarks.Data.Models
+{
+    public class User : IdentityUser
+    {
+        public string Name { get; set; }
+    }
+}
