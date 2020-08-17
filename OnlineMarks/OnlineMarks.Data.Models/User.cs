676913698211c1351115e206149eb4d2ce136202
malesdejan54 <malesdejan54@gmail.com>
2020-08-17 16:55:51 +0200

    LI-53

--- OnlineMarks/OnlineMarks.Data.Models/User.cs
+++ OnlineMarks/OnlineMarks.Data.Models/User.cs
@@ -2,6 +2,7 @@
 using OnlineMarks.Tools.Enums;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace OnlineMarks.Data.Models
@@ -9,7 +10,11 @@ namespace OnlineMarks.Data.Models
     public class User
     {   
         public Guid Id { get; set; }
+        
+        [Required]
         public string Name { get; set; } // IdentityUser
+        [Required]
+        [StringLength(8,MinimumLength = 4,ErrorMessage ="You must specify password between 4 and 8 characters")]     
         public string Password { get; set; }
         public UserRole Role { get; set; }
     }
