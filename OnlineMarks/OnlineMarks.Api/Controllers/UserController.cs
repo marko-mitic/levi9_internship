460ec6cc781c80555eb712f406638346c6c7338d
Luka <dracaluka12@gmail.com>
2020-08-17 11:42:19 +0200

    Implemented add user.

---
+++ OnlineMarks/OnlineMarks.Api/Controllers/UserController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using OnlineMarks.Data.Models;
+using OnlineMarks.Data.ViewModels.Auth;
+using OnlineMarks.Data.ViewModels.Users;
+using OnlineMarks.Interfaces.Services;
+using OnlineMarks.Tools.Enums;
+using System;
+
+namespace OnlineMarks.Api.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var users = _userService.GetAll();
+            return Ok(users);
+        }
+
+        [AllowAnonymous]
+        [HttpGet("authenticate")]
+        public IActionResult Authenticate([FromBody]AutheticateModel model)
+        {
+            var users = _userService.Authenticate(model);
+            return Ok(users);
+        }
+        
+        /*[AllowAnonymous]
+        [HttpGet("id")]
+        public IActionResult Get([FromBody] AutheticateModel model)
+        {
+            var user = _userService.GetById();
+            return Ok(user);
+        }*/
+
+        [AllowAnonymous]
+        [HttpPost]
+
+        public IActionResult Add([FromBody] AutheticateModel model)
+        {
+            _userService.Add(model.Username, model.Password);
+            return Ok("You have successfully added a User!");
+        }
+    }
+}
---
+++ OnlineMarks/OnlineMarks.Services/UserService.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using OnlineMarks.Data.Models;
+using OnlineMarks.Data.Repositories;
+using OnlineMarks.Data.ViewModels.Auth;
+using OnlineMarks.Data.ViewModels.Users;
+using OnlineMarks.Interfaces.Maps;
+using OnlineMarks.Interfaces.Repository;
+using OnlineMarks.Interfaces.Services;
+using OnlineMarks.Tools.ConfigurationObjects;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+
+namespace OnlineMarks.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IUserViewUserMap _userViewUserMap;
+
+        private readonly IConfiguration _configuration;
+
+        public UserService(IUserRepository userRepository, IUserViewUserMap userViewUserMap, IConfiguration configuration)
+        {
+            _userRepository = userRepository;
+            _userViewUserMap = userViewUserMap;
+            _configuration = configuration;
+        }
+
+        public void Add(string username, string password)
+        {
+            var user = new User() { Name = username, Password = password, Role = Tools.Enums.UserRole.User, Id = Guid.NewGuid() };
+            _userRepository.Add(user);
+        }
+
+        public UserView Authenticate(AutheticateModel model) // JWT
+        {
+            var user = _userRepository.Authenticate(model.Username, model.Password);
+
+            // return null if user not found
+            if (user == null)
+                return null;
+
+            // authentication successful so generate jwt token
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_configuration["SecretKey"]);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                  new Claim(ClaimTypes.Name, user.Id.ToString()),
+                  new Claim(ClaimTypes.Role, user.Role.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddDays(30),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            var userModel = _userViewUserMap.Translate(user);
+
+            userModel.Token = tokenHandler.WriteToken(token);
+
+            return userModel;
+        }
+
+        public IEnumerable<UserView> GetAll() // ExtensionMethods where added here
+        {
+            var userList = _userRepository.GetAll();
+            return _userViewUserMap.Translate(userList);
+        }
+
+        public UserView GetById(Guid id) // ExtensionMethods where added here
+        {
+            var user = _userRepository.Get(id);
+            return _userViewUserMap.Translate(user);
+        }
+    }
+}
