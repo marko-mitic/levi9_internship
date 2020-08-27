db8f0e33506b1536f440c18194b4f95acb98b77b
ognjengari <ognjengari@gmail.com>
2020-08-27 15:03:37 +0200

    message persistance, join/leave room

--- OnlineMarks/OnlineMarks.Api/Hubs/MessageHub.cs
+++ OnlineMarks/OnlineMarks.Api/Hubs/MessageHub.cs
@@ -1,10 +1,34 @@
 using Microsoft.AspNetCore.SignalR;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using OnlineMarks.Interfaces.Services;
+using System;
 
+[Authorize]
 public class MessageHub : Hub<IClient>
 {   
-    public async Task SendMessage(string user, string message)
+
+    private IMessageService _messageService;
+
+    public MessageHub(IMessageService messageService){
+        _messageService = messageService ?? throw new ArgumentNullException($"{nameof(IMessageService)} cannot be null!");
+    }
+
+    public async Task SendMessage(string receiverName, string content)
+    {  
+        var senderName = Context.User.Identity.Name;
+        _messageService.Add(senderName, receiverName, content);
+
+        await Clients.All.ReceiveMessage(senderName, content);
+    }
+
+    public Task JoinRoom(string roomName)
     {
-        await Clients.All.ReceiveMessage(user, message);
+        return Groups.AddToGroupAsync(Context.ConnectionId, roomName);
+    }
+
+    public Task LeaveRoom(string roomName)
+    {
+        return Groups.RemoveFromGroupAsync(Context.ConnectionId, roomName);
     }
 }
